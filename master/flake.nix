4cc0e69a309945398d2e6413456fcfb5ede954e1
hugosenari <hugosenari@gmail.com>
2022-03-12 01:00:49 -0300

    chore: re index cassandra

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Bindings to Cassandra DB driver'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-cassandra-master.flake = false;
+  inputs.src-cassandra-master.owner = "yglukhov";
+  inputs.src-cassandra-master.ref   = "refs/heads/master";
+  inputs.src-cassandra-master.repo  = "cassandra";
+  inputs.src-cassandra-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-cassandra-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-cassandra-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
