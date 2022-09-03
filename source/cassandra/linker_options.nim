3eff4bbd5a39deeb455e1db26683985d4a1ab587
hugosenari <hugosenari@gmail.com>
2022-09-03 19:56:07 -0300

    feat(nimpkgs): update lock

--- source/cassandra/linker_options.nim
+++
@@ -1,9 +0,0 @@
-when defined(linux):
-    {.passL: "-lpthread".}
-    {.passL: "-lm".}
-    {.passL: "-lstdc++".}
-elif defined(macosx):
-    {.passL: "-lc++".}
-
-{.passL: "-lcassandra_static".}
-{.passL: "-luv".}
